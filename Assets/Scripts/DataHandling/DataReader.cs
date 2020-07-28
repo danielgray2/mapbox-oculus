beb628b78ae48a76d43f38a64546839ce5f61a24
danielgray2 <39307681+danielgray2@users.noreply.github.com>
2020-07-28 09:03:24 -0600

    Committed only the new code

--- Assets/Scripts/DataHandling/DataReader.cs
+++ Assets/Scripts/DataHandling/DataReader.cs
@@ -22,69 +22,12 @@ public class DataReader : MonoBehaviour
 
     void Start()
     {
-        
-        //ReadSeismicData();
-        //ReadVeloData();
-        
-        //GameObject plotAnimation = new GameObject();
-        //plotAnimation.transform.position = Vector3.zero;
-        //TimeSeries ts = plotAnimation.AddComponent<TimeSeries>();
-        //ts.dataPointPrefab = dataPointPrefab;
-        //ts.BeginTimeAnimation(DataStore.Instance.sDataRecords.ElementAt(0).dateTime, 1f, gradient);
-        //ts.BeginIndexedAnimation(0, 70, 1f, sphereColorGradient);
-        //ts.BeginScrubbedAnimation(scrubbingMenu, gradient);
+        ReadCSV("Assets\\path\\to\\csv", "data name");
     }
 
     private void ReadCSV(string pathToCsv, string name)
     {
         DataFrame csvDataFrame = DataFrame.LoadCsv(pathToCsv);
         DataStore.Instance.AddDataSet(csvDataFrame, name);
-        /*
-        using (var reader = new StreamReader("Assets\\Resources\\coso.csv"))
-        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-        {
-            List<SData> sDataRecords = new List<SData>();
-            csv.Configuration.RegisterClassMap<CSVToObjectMap>();
-            var sDataEnumberable = csv.GetRecords<SData>();
-            foreach (SData sData in sDataEnumberable)
-            {
-                sData.SetDateTime();
-                sData.SetLatLon();
-                sDataRecords.Add(sData);
-            }
-            DataStore.Instance.SetSDataRecords(sDataRecords);
-        }
-        */
-    }
-
-    private void ReadVeloData()
-    {
-        using (var reader = new StreamReader("Assets\\Resources\\coso_velo.csv"))
-        using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-        {
-            List<VData> vDataRecords = new List<VData>();
-            csv.Configuration.RegisterClassMap<CSVToVData>();
-            var vDataEnumerable = csv.GetRecords<VData>();
-            foreach (VData vData in vDataEnumerable)
-            {
-                if (CheckBounds(vData.lat, vData.lon) && vData.depth > -2f)
-                {
-                    vData.SetLatLon();
-                    vDataRecords.Add(vData);
-                }
-                
-            }
-            DataStore.Instance.SetVDataRecords(vDataRecords);
-        }
-    }
-
-    private bool CheckBounds(float lat, float lon)
-    {
-        if (lat < DataStore.Instance.minLat ||
-           lat > DataStore.Instance.maxLat ||
-           lon < DataStore.Instance.minLon ||
-           lon > DataStore.Instance.maxLon)
-            return false;
-        return true;
     }
 }
